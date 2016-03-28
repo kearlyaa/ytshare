b60c8d1cc90d1c54e9c70dc0974dc5b58ad7e1a1
Aaron K <kearlyaa@gmail.com>
2016-03-28 18:20:41 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ytshare::Application.config.secret_key_base = '9a1882a1c1b278aacf0c44188ab97a9c4624f69f329a62230aa84236d8c9c50dec3e6f6ad301557c12cf05db732ebdfba8c082a4d3d6a04f183435c216c324e2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ytshare::Application.config.session_store :cookie_store, key: '_ytshare_session'
